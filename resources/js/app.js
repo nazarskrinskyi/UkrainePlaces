d15013b029d07c146da5cbdc60c20dc3fa973d43
nazar <nazar.skrinskyi@drumncode.com>
2025-02-22 17:02:00 +0000

    added admin and init start routes

--- resources/js/app.js
+++ resources/js/app.js
@@ -160,8 +160,6 @@ const editorConfig = {
             'resizeImage'
         ]
     },
-    initialData:
-        '<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n\tYou\'ve successfully created a CKEditor 5 project. This powerful text editor\n\twill enhance your application, enabling rich text editing capabilities that\n\tare customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n\t<li>\n\t\t<strong>Integrate into your app</strong>: time to bring the editing into\n\t\tyour application. Take the code you created and add to your application.\n\t</li>\n\t<li>\n\t\t<strong>Explore features:</strong> Experiment with different plugins and\n\t\ttoolbar options to discover what works best for your needs.\n\t</li>\n\t<li>\n\t\t<strong>Customize your editor:</strong> Tailor the editor\'s\n\t\tconfiguration to match your application\'s style and requirements. Or\n\t\teven write your plugin!\n\t</li>\n</ol>\n<p>\n\tKeep experimenting, and don\'t hesitate to push the boundaries of what you\n\tcan achieve with CKEditor 5. Your feedback is invaluable to us as we strive\n\tto improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n\t<li>📝 <a href="https://portal.ckeditor.com/checkout?plan=free">Trial sign up</a>,</li>\n\t<li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n\t<li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n\t<li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n\t<li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n\tSee this text, but the editor is not starting up? Check the browser\'s\n\tconsole for clues and guidance. It may be related to an incorrect license\n\tkey if you use premium features or another feature-related requirement. If\n\tyou cannot make it work, file a GitHub issue, and we will help as soon as\n\tpossible!\n</p>\n',
     language: 'uk',
     licenseKey: LICENSE_KEY,
     link: {
@@ -190,21 +188,17 @@ const editorConfig = {
     menuBar: {
         isVisible: true
     },
-    placeholder: 'Type or paste your content here!',
     table: {
         contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
-    }
-};
-
-ClassicEditor.create(document.querySelector('#editor'), {
+    },
     simpleUpload: {
         uploadUrl: '/ckeditor/upload',
+        headers: {
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        }
     },
-    licenseKey: 'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NzE3MTgzOTksImp0aSI6ImY0OGEyMmRhLWViZDQtNDQ3Ni05Y2JkLWUyMjkxNzM2YjY2OCIsImxpY2Vuc2VkSG9zdHMiOlsiMTI3LjAuMC4xIiwibG9jYWxob3N0IiwiMTkyLjE2OC4qLioiLCIxMC4qLiouKiIsIjE3Mi4qLiouKiIsIioudGVzdCIsIioubG9jYWxob3N0IiwiKi5sb2NhbCJdLCJ1c2FnZUVuZHBvaW50IjoiaHR0cHM6Ly9wcm94eS1ldmVudC5ja2VkaXRvci5jb20iLCJkaXN0cmlidXRpb25DaGFubmVsIjpbImNsb3VkIiwiZHJ1cGFsIl0sImxpY2Vuc2VUeXBlIjoiZGV2ZWxvcG1lbnQiLCJmZWF0dXJlcyI6WyJEUlVQIl0sInZjIjoiNmE5YzljYTEifQ.Axe6ItHHrh9nIWYw9HCWCXLz4jVpk2v8owycDT1liXgH-ftYqOwDZWA4AiaHmhcZXkm3JhtaSH5GAht6ov_ujQ',
-    toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'uploadImage' ]
-})
-    .catch(error => {
-        console.error('CKEditor Error:', error);
-    });
+};
+
+ClassicEditor.create(document.querySelector('#editor'), editorConfig).then(r => console.log(r));
 
 Alpine.start();
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\Place\CreatePlaceController;
+use App\Http\Controllers\Admin\Place\DeletePlaceController;
+use App\Http\Controllers\Admin\Place\EditPlaceController;
 use App\Http\Controllers\CKEditorUploadController;
+use App\Http\Controllers\Place\IndexPlaceController;
+use App\Http\Controllers\Place\ShowPlaceController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +24,20 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::post('/ckeditor/upload', [CKEditorUploadController::class, 'upload'])->name('ckeditor.upload');
+
+Route::get('/places/{id}', [ShowPlaceController::class, 'index'])->name('place.show');
+
+Route::get('/places/{city?}', [IndexPlaceController::class, 'index'])->name('place.index');
+
+Route::get('/places/create', [CreatePlaceController::class, 'index'])->name('place.create');
+
+Route::get('/places/edit/{id}', [EditPlaceController::class, 'index'])->name('place.edit');
+
+Route::get('/places/delete/{id}', [DeletePlaceController::class, 'index'])->name('place.delete');
+
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+})->middleware(['auth', 'verified'])->name('admin.dashboard');
+
 
 require __DIR__.'/auth.php';
